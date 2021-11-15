510332d4c3f372e442045e70b6d21653a634fc20
Phatthana Tongon <phatthana@me.com>
2021-11-16 03:05:37 +0700

    add kucoin

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ dotenv.config();
 const express = require('express')
 const app = express()
 const binance = require('./binance')
+const kucoin = require('./kucoin')
 
 app.post('/log', function (req, res) {
     req.rawBody = '';
@@ -31,14 +32,21 @@ app.post('/bot', async (req, res) => {
         console.log(req.rawBody);
         // Buy:50 BINANCE:ETH
         // Sell:50 BINANCE:ETH
+
+        let cex = {
+            "BINANCE": binance,
+            "KUCOIN": kucoin
+        }
+
         const command = req.rawBody.split(" ")
         const side = command[0].split(":")
         const amount = side.length > 1? side[1]: 30
         const ticker = command[1].split(":")
+        const source = ticker[0]
         if (side[0] === 'Buy') {
-            binance.buy(ticker[1], amount)
+            cex[source].buy(ticker[1], amount)
         } else if (side[0] === 'Sell') {
-            binance.sell(ticker[1])
+            cex[source].sell(ticker[1])
         }
         res.send('ok')
     });    
---
+++ kucoin.js
@@ -0,0 +1,81 @@
+const Decimal = require('decimal.js');
+const API = require('kucoin-node-sdk');
+API.init({
+    baseUrl: 'https://api.kucoin.com',
+    apiAuth: {
+        key: process.env.KUCOIN_KEY,
+        secret: process.env.KUCOIN_SECRET,
+        passphrase: process.env.KUCOIN_PASSPHRASE,
+    },
+});
+
+const FIAT = 'USDT'
+const MIN_TRADE = 30
+
+async function buy(symbol, minTrade=MIN_TRADE) {
+    let fiatBalance = new Decimal(await _getBalance('USDT'))
+    if (fiatBalance < minTrade) return;
+
+    try {
+        const oID = parseInt(new Date().getTime() / 1000)
+        const tradeSize = new Decimal(minTrade)
+        let response = await API.rest.Trade.Orders.postOrder({
+            clientOid: `${oID}`,
+            side: 'buy',
+            symbol: symbol.replace(FIAT, `-${FIAT}`),
+            type: 'market',
+            size: tradeSize.toString()
+        })
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+async function sell(symbol) {
+    const asset = symbol.replace('USDT', '')
+    let balanceAsset = new Decimal(await _getBalance(asset))
+    // const lotSize = await _getLotSize(symbol)
+    // const lotStepSize = new Decimal(lotSize.stepSize)
+    // const lotMinQty = new Decimal(lotSize.minQty)
+    // if (balanceAsset < lotMinQty) {
+    //     console.log(`WARNING: NOT ENOUGH AMOUNT: ${balanceAsset}, MIN: ${lotMinQty}`);
+    //     return;
+    // }
+
+    // try {
+    //     let quantity = balanceAsset.sub(balanceAsset.mod(lotStepSize))
+    //     let order = await client.newOrder(symbol, 'SELL', 'MARKET', {quantity})    
+    //     console.log(`${symbol} RECEV BUSD ${order.data.cummulativeQuoteQty}`);
+    // } catch (error) {
+    //     console.log(error);
+    // }
+}
+
+async function _getBalance(asset) {
+    
+    let response = await API.rest.User.Account.getAccountsList({type: 'trade'})
+    let balances = response.data
+    // console.log(response);
+    for (const balance of balances) {
+        if (asset == balance.currency) {
+            return parseFloat(balance.available)
+        }
+    }
+    return 0
+}
+
+async function _getLotSize(symbol) {
+    let exchangeInfo = await client.exchangeInfo({symbol})
+    let symbolData = exchangeInfo.data.symbols[0]
+    let filters = symbolData.filters
+    for (const f of filters) {
+        if (f.filterType === 'LOT_SIZE') {
+            return f
+        }
+    }
+}
+
+exports.buy = buy
+exports.sell = sell
